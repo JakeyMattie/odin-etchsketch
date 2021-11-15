53a740bfe088f6a878713eb9d6978f57b5fd4c89
Jake Antonio <jakemattantonio@gmail.com>
2021-11-15 11:37:54 +1100

    Refactored grid

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+function makeGrid(rowCol) {
+  container.style.setProperty('--grid-rows', rowCol);
+  container.style.setProperty('--grid-cols', rowCol);
+  for (c = 0; c < (rowCol * rowCol); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(16, 16);
+makeGrid(16);
