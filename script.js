e48a29697faff89de8f9e1bfe1a14a15ab814630
Jake Antonio <jakemattantonio@gmail.com>
2021-11-17 13:45:52 +1100

    Added Clear and Gradient buttons

--- script.js
+++ script.js
@@ -32,6 +32,20 @@ function rainbowCanvas() {
     });
 }
 
+function gradientCanvas() {
+    let gradientVal = 0.05;
+    let randomColor = generateColor();
+
+    const cells = document.querySelectorAll(".grid-item");
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.setProperty('background-color', randomColor);
+            cell.style.setProperty('opacity', gradientVal);
+            gradientVal += 0.05;
+        });
+    });
+}
+
 function eraseCanvas() {
     const cells = document.querySelectorAll(".grid-item");
     cells.forEach((cell) => {
@@ -41,13 +55,25 @@ function eraseCanvas() {
     });
 }
 
+function clearCanvas() { //Sets the canvas to white first before resets with a new grid size
+    const cells = document.querySelectorAll(".grid-item");
+    cells.forEach((cell) => {
+        cell.style.setProperty('background-color', 'white');
+    });
+
+    let input = prompt("Enter your grid size: ");
+    makeGrid(input);
+}
+
 makeGrid(16);
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        if (button.id == "sketch") sketchCanvas();
-        else if (button.id == "rainbow") rainbowCanvas();
-        else if (button.id == "erase") eraseCanvas();
+        if (button.id == "sketchBtn") sketchCanvas();
+        else if (button.id == "rainbowBtn") rainbowCanvas();
+        else if (button.id == "gradientBtn") gradientCanvas();
+        else if (button.id == "eraseBtn") eraseCanvas();
+        else if (button.id == "clearBtn") clearCanvas();
     });
 });
