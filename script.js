12fd195d50587f44dbfd28f1c749e2f82816871a
Jake Antonio <jakemattantonio@gmail.com>
2021-11-17 14:44:16 +1100

    Styled buttons

--- script.js
+++ script.js
@@ -46,6 +46,25 @@ function gradientCanvas() {
     });
 }
 
+// function hexToRGB(h) {
+//     let r = 0, g = 0, b = 0;
+  
+//     // 3 digits
+//     if (h.length == 4) {
+//       r = "0x" + h[1] + h[1];
+//       g = "0x" + h[2] + h[2];
+//       b = "0x" + h[3] + h[3];
+  
+//     // 6 digits
+//     } else if (h.length == 7) {
+//       r = "0x" + h[1] + h[2];
+//       g = "0x" + h[3] + h[4];
+//       b = "0x" + h[5] + h[6];
+//     }
+    
+//     return "rgb("+ +r + "," + +g + "," + +b + ")";
+// }
+
 function eraseCanvas() {
     const cells = document.querySelectorAll(".grid-item");
     cells.forEach((cell) => {
@@ -69,6 +88,7 @@ makeGrid(16);
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
+    button.textContent = button.textContent.toUpperCase();
     button.addEventListener('click', () => {
         if (button.id == "sketchBtn") sketchCanvas();
         else if (button.id == "rainbowBtn") rainbowCanvas();
